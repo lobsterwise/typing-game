bc0f22a9c1b99ea7d6050f5a7a8702813a37fa29
Logan R <64157688+lobsterwise@users.noreply.github.com>
2022-09-06 07:59:32 -0400

    Getting started

--- components/key.tsx
+++ components/key.tsx
@@ -1,13 +1,15 @@
 export default function Key(props: { character: string, size: string }) {
 	return <div style={{
-	//	width: props.size,
-	//	height: props.size,
+		width: props.size,
+		height: props.size,
 		borderStyle: "solid",
 		borderWidth: "5px",
 		borderRadius: "10px",
-		textAlign: "center",
+	//	textAlign: "center",
 		padding: `calc(${props.size} / 3)`,
-		display: "inline"
+		display: "flex",
+		justifyContent: "center",
+		alignItems: "center"
 	}}>
 		{ props.character }
 	</div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,28 @@
 import React from "react";
 import Key from "../components/key";
 
-export default function HomePage() {
+function Keyboard(): JSX.Element {
+	return <div>
+		YAY
+	</div>;
+}
+
+export default function HomePage(props: { keyboard: JSX.Element }): JSX.Element {
 	const [ likes, setLikes ] = React.useState(0);
 	return <div>
 		<h1>WELCOME</h1>
 		<button onClick={() => {setLikes(likes + 1)}}>
 			Like { likes }
 		</button>
+		{ props.keyboard }
 		<Key character="k" size="20px" />
 	</div>;
 }
+
+// export function getStaticProps() {
+// 	let props = {
+// 		keyboard: Keyboard()
+// 	};
+
+// 	return { props };
+// }
